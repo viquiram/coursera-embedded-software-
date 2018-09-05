1a49c8bbfa36fb2b0cf77d2a88474feee85f30f6
Viviana Quirama <22740469+viquiram@users.noreply.github.com>
2018-09-06 00:35:20 +0200

    Creating the Makefile following the C1M2 Assessment instructions.

--- c1/m2/src/sources.mk
+++ c1/m2/src/sources.mk
@@ -1,17 +1,17 @@
-#******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#*****************************************************************************
+# Copyright (C) 2018 by Viviana Quirama
 #
 # Redistribution, modification or use of this software in source or binary
-# forms is permitted as long as the files maintain this copyright. Users are 
+# forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
+# software. The author is not liable for any misuse of this material.
 #
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = main.c \
+	  memory.c
 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES = ../include/common
 
